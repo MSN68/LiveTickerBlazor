2a1c03fe8e82a9d7b46d05df27077c21ebfde69a
M SN <m.salehi1990@gmail.com>
2025-05-18 16:06:27 +0330

    final edition

--- SignalRServer/Program.cs
+++ SignalRServer/Program.cs
@@ -14,23 +14,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//});
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
@@ -41,7 +24,4 @@ app.UseEndpoints(endpoints =>
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
